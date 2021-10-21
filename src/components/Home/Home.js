94cba129133d30df17faaaa0c8979b20c81b3ea8
aminsyahmi-besquare <92085350+aminsyahmii@users.noreply.github.com>
2021-10-21 12:19:49 +0800

    Amonhack (#4)
    * updates
    
    * updated Home Page
    
    * yes
    
    * yes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
-import Navigation from "./Navigation";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import Features from "./Features";
+import MainContent from "./MainContent";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg">
+      <MainContent></MainContent>
+      <Features></Features>
+    </div>
+  );
 };
 
 export default Home;
