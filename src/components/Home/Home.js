7590c7dcbad592e988e6c2b2dfe773e7998d3bf9
Ameerul Zul Hadi <ameerulzhbusiness@gmail.com>
2021-10-21 15:03:33 +0800

    added hero of the day

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,14 @@ import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import Features from "./Features";
 import MainContent from "./MainContent";
 import "./Home.css";
+import HeroDisplay from "./HeroDisplay";
 
 const Home = () => {
   return (
     <div className="bg">
       <MainContent></MainContent>
       <Features></Features>
+      <HeroDisplay></HeroDisplay>
     </div>
   );
 };
