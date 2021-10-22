22bfe0d99ae484637e96c1b3a0c6a909f804db54
Ameerul Zul Hadi <ameerulzhbusiness@gmail.com>
2021-10-22 14:53:12 +0800

    website fix

--- src/service/CharacterDetails.js
+++ src/service/CharacterDetails.js
@@ -5,20 +5,18 @@ export const CharacterDetails = () => {
   const url = `https://akabab.github.io/superhero-api/api/all.json`;
   const [characters, setCharacters] = useState([]);
 
-  const getCharacters = async () => {
-    const res = await axios
-      .get(url)
-      .catch((err) => console.log("Error: ", err));
-
-    if (res && res.data) {
-      setCharacters(res.data);
-    }
-  };
-
   useEffect(() => {
+    const getCharacters = async () => {
+      const res = await axios
+        .get(url)
+        .catch((err) => console.log("Error: ", err));
+
+      if (res && res.data) {
+        setCharacters(res.data);
+      }
+    };
     getCharacters();
   }, [url]);
 
-  console.log(characters);
   return characters;
 };
